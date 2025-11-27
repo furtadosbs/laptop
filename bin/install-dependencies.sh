e491f08c2276e1057d724bf7a1b4597ba9601338
Lucas Oliveira <hub.lucasoliveira@gmail.com>
2025-11-27 09:42:19 -0300

    Add sudo to scrits

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -1,3 +1,3 @@
 	#!/bin/bash
 
-apt-get -y install build-essential rustc libssl-dev libyaml-dev zlib1g-dev libgmp-dev git
+sudo apt-get -y install build-essential rustc libssl-dev libyaml-dev zlib1g-dev libgmp-dev git
