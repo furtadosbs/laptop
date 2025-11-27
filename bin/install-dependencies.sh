307c6b7ce9bc09128a3c2c78073b17a02dfc16a3
Lucas Oliveira <hub.lucasoliveira@gmail.com>
2025-11-27 09:29:51 -0300

    Add -y option in install dependencies

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -1,3 +1,3 @@
 	#!/bin/bash
 
-apt-get install build-essential rustc libssl-dev libyaml-dev zlib1g-dev libgmp-dev git
+apt-get -y install build-essential rustc libssl-dev libyaml-dev zlib1g-dev libgmp-dev git
